8d7e5448501f62edb5ae22f36033e9b07b1ebb19
Joshua Huseman <jhuseman@nd.edu>
2019-03-10 23:57:57 -0500

    Added distutils setup script.

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# Joshua Huseman, jhuseman@nd.edu
-# 06/10/2016
-# 03/10/2019 - updated for standalone library, Guavacado rebrand
-
-# __init__.py
-#  init file for Guavacado package
-from Guavacado import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Guavacado',
+	version='1.1.2',
+	description='Simple Web API Hosting Library',
+	author='Joshua Huseman',
+	author_email='jhuseman@alumni.nd.edu',
+	url='https://github.com/jhuseman/Guavacado',
+	py_modules=['Guavacado'],
+)
